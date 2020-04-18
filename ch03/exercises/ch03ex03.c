cd13e294357aae85e6b5cb57d13edf4cd8d656f6
Piyush Malviya <piyushmalvia2@gmail.com>
2020-04-18 19:33:54 +0530

    updated by piyush 18 april from stackoverflow because the solution was incorrect

--- ch03/exercises/ch03ex03.c
+++ ch03/exercises/ch03ex03.c
@@ -11,62 +11,9 @@
   be distinguished.
 */
 
-#include <stdio.h>
-
-int main(void)
-{
-  int x = 1;
-  int y = 2;
-  int z = 3;
-
-  float a = 10.0;
-  float b = 20.0;
-
-  // (a) "%d" versus " %d"
-  printf("(a) \"%%d\" versus \" %%d\"\tNot equivalent\n\n");
-  printf("\t \"%%d\" = %d\n", x);
-  printf("\t\" %%d\" =  %d\n\n", x); 
-
-  // (b) "%d-%d-%d" versus "%d -%d -%d"
-  printf("(b) \"%%d-%%-d-%%d\" versus \"%%d -%%d -%%d\"\tNot equivalent\n\n");
-  printf("\t   \"%%d-%%d-%%d\" = %d-%d-%d\n", x, y, z);
-  printf("\t\"%%d -%%d - %%d\" = %d -%d -%d\n\n", x, y, z);
-
-  // (c) "%f" versus "%f "
-  printf("(c) \"%%f\" versus \"%%f \"\tLooks equivalent to me\n\n");
-  printf("\t \"%%f\" = %f\n", a);
-  printf("\t\"%%f \" = %f \n\n", a);
-
-  // (d) "%f,%f" versus "%f, %f"
-  printf("(d) \"%%f,%%f\" versus \"%%f, %%f\"\tNot equivalent\n\n");
-  printf("\t \"%%f,%%f\" = %f,%f\n", a, b);
-  printf("\t\"%%f, %%f\" = %f, %f\n\n", a, b);
-
-  return 0;
-}
-
-/* Program Execution
-
-$ gcc ch03ex03.c 
-$ ./a.out
-(a) "%d" versus " %d"	Not equivalent
-
-	 "%d" = 1
-	" %d" =  1
-
-(b) "%d-%-d-%d" versus "%d -%d -%d"	Not equivalent
-
-	   "%d-%d-%d" = 1-2-3
-	"%d -%d - %d" = 1 -2 -3
-
-(c) "%f" versus "%f "	Looks equivalent to me
-
-	 "%f" = 10.000000
-	"%f " = 10.000000 
-
-(d) "%f,%f" versus "%f, %f"	Not equivalent
-
-	 "%f,%f" = 10.000000,20.000000
-	"%f, %f" = 10.000000, 20.000000
-
+/*
+(a) they are equivalent since scanf discards the white space. 
+(b) they are not equivalent since scanf matches - with white space . 
+(c), they are not equivalent since scanf will put back the white space in the buffer. 
+(d), they are equivalent since scanf discards white space.
 */
