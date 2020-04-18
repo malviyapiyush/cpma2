1144728f78f34d9f77c3e4cadcd98f9d85a4c85a
Piyush Malviya <piyushmalvia2@gmail.com>
2020-04-18 19:55:39 +0530

    updated solution by pm

--- ch03/exercises/ch03ex04.c
+++ ch03/exercises/ch03ex04.c
@@ -19,10 +19,10 @@
   (Assume that i and j are int variables and x is a float variable)
 
   My answer:
-    1. Conversion specification %d: scanf reads character 1 and determines that 
+    1. Conversion specification %d: scanf reads character 1 and determines that
        integers can contain a 1, so it reads the next character 0, which integers
        can also contain. scanf reads the next character . and determines that integers
-       cannot contain a decimal, so the decimal character (.) is put back. 
+       cannot contain a decimal, so the decimal character (.) is put back.
        The value of i is 10
 
     2. Conversion specification %f: scanf reads the decimal character (.) and determines
@@ -37,7 +37,7 @@
        The value of j is 5
 
     4. Because all three conversion specifiers have been filled, the remaining input
-       character of 6 is ingored.
+       character of 6 is put back into the buffer for next call of scanf.
 
     5. Final answer is:
          i = 10
@@ -47,36 +47,41 @@
        Note: if a float doesn't contain a non-zero value before the decimal point, C prints
              a 0 before the decimal point. If a float conversion specifier doesn't contain
              any format specifications, C prints decimals to six places after the decimal
-             and adds zeroes where needed by default. 
+             and adds zeroes where needed by default.
 
 */
 
-#include <stdio.h>
-
 int main(void)
 {
-  int i, j;
+  int i, j, k;
   float x;
 
   printf("Enter the numbers 10.3, 5, and 6 (without the commas): ");
   scanf("%d%f%d", &i, &x, &j);
 
-  printf("The numbers read using scanf(\"%%d%%f%%d\", &i, &x, &j) are:\n");
+  printf("The numbers read using scanf(\"%%d%%f%%d\", &i, &x, &j) are: \n");
   printf("i = %d\n", i);
   printf("x = %f\n", x);
-  printf("j = %d\n\n", j); 
+  printf("j = %d\n\n", j);
+
+  printf("Since, the value 6 is not read and put back into the input buffer on the first call of scanf \n lets call another scanf(\"%%d\",&k) to store this value into k \n but no user input to be made this time because the value would be going from the input buffer instead ");
+  scanf("%d",&k);
+  printf("\n\nk = %d",k);
 
   return 0;
 }
 
 /* Program Execution
 
-$ gcc -Wall ch03ex04.c
-$ ./a.out
 Enter the numbers 10.3, 5, and 6 (without the commas): 10.3 5 6
 The numbers read using scanf("%d%f%d", &i, &x, &j) are:
 i = 10
 x = 0.300000
 j = 5
 
+Since, the value 6 is not read and put back into the input buffer on the first call of scanf
+ lets call another scanf("%d",&k) to store this value into k
+ but no user input to be made this time because the value would be going from the input buffer instead
+
+k = 6
 */
